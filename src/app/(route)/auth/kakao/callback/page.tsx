a45b353e80526930f0373daefaccc7c1e8a5cd37
suhyeon0111 <soo4833@naver.com>
2026-04-08 15:53:07 +0900

    fix(build): 이전 코드 되돌림

--- src/app/(route)/auth/kakao/callback/page.tsx
+++ src/app/(route)/auth/kakao/callback/page.tsx
@@ -1,7 +1,41 @@
-import { KakaoContainer } from "./_components";
+"use client";
+
+import { useEffect, useRef } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useApiKakaoLogin } from "@/api/fetch/auth";
+import { Icon } from "@/components/common";
 
 const KakaoCallbackPage = () => {
-  return <KakaoContainer />;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const code = searchParams.get("code");
+
+  const isRequesting = useRef(false);
+
+  const { mutate: KakaoLoginMutate } = useApiKakaoLogin();
+
+  const appEnv = process.env.NODE_ENV === "production" ? "prod" : "dev";
+
+  useEffect(() => {
+    if (!code) return;
+    if (isRequesting.current) return;
+
+    isRequesting.current = true;
+
+    KakaoLoginMutate({
+      code: code,
+      environment: appEnv,
+    });
+  }, [code, KakaoLoginMutate, router]);
+
+  return (
+    <div className="flex min-h-screen w-full flex-col-center">
+      <div className="flex flex-col items-center gap-4">
+        <Icon name="Loading" className="animate-spin" size={40} />
+        <p className="text-body1-regular text-gray-700">로그인 요청 중...</p>
+      </div>
+    </div>
+  );
 };
 
 export default KakaoCallbackPage;
