623c075e35377a3c1081a1c91d7560906a16ee8b
suhyeon0111 <soo4833@naver.com>
2026-04-11 01:27:17 +0900

    feat(kakao): 회원탈퇴 모달 추가

--- src/app/(route)/mypage/delete-account/_components/DeleteAccountContainer/DeleteAccountContainer.tsx
+++ src/app/(route)/mypage/delete-account/_components/DeleteAccountContainer/DeleteAccountContainer.tsx
@@ -5,28 +5,21 @@ import DeleteAccountReason from "../DeleteAccountReason/DeleteAccountReason";
 import DeleteAccountPassword from "../DeleteAccountPassword/DeleteAccountPassword";
 import { useState } from "react";
 import { useGetUsersMe } from "@/api/fetch/user";
-import { useFormContext } from "react-hook-form";
 
 const DeleteAccountContainer = () => {
   const { data: UserData } = useGetUsersMe();
-  const { handleSubmit } = useFormContext();
   const { socialUser } = UserData?.result ?? {};
 
   const [state, setState] = useState<number>(1);
 
   const handleToNext = () => {
-    if (socialUser) {
-      handleSubmit((data) => {
-        const formElement = document.querySelector("form");
-        formElement?.requestSubmit();
-      })();
-    }
+    setState(2);
   };
 
   return (
     <section>
       <h2 className="sr-only">탈퇴 선택 영역</h2>
-      {state === 1 && <DeleteAccountReason onNext={handleToNext} />}
+      {state === 1 && <DeleteAccountReason onNext={handleToNext} socialUser={socialUser} />}
 
       {state === 2 && <DeleteAccountPassword onBack={() => setState(1)} />}
     </section>
