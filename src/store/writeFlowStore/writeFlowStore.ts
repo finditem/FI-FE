560d243d23be9658dff80ebb2e79a3ff56f65992
wlrnjs <wlrnjs5824@gmail.com>
2026-04-21 16:33:18 +0900

    docs(store): 스토어 TSDoc 업데이트

--- src/store/writeFlowStore/writeFlowStore.ts
+++ src/store/writeFlowStore/writeFlowStore.ts
@@ -1,10 +1,21 @@
 import { create } from "zustand";
 
+/**
+ * 게시글 작성 플로우 UI 상태를 관리하는 Zustand 스토어입니다.
+ *
+ * @author jikwon
+ */
+
 type WriteFlowProps = {
+  /** 임시저장 모달 표시 여부 */
   tempModalShown: boolean;
+  /** 임시저장 모달 표시 여부 설정 */
   setTempModalShown: (v: boolean) => void;
+  /** 작성 가이드 팝업 표시 여부 */
   showManualPopup: boolean;
+  /** 작성 가이드 팝업 표시 여부 설정 */
   setShowManualPopup: (v: boolean) => void;
+  /** 작성 플로우 상태 초기화 */
   resetWriteFlow: () => void;
 };
 
