b738f23084cc3be2ed2a79432970c5f501ae50ef
wlrnjs <wlrnjs5824@gmail.com>
2026-04-27 18:06:25 +0900

    a11y(list-detail): 게시글 상세페이지 지도 h1 태그 누락 사항 반영

--- src/app/(route)/list/[id]/map/page.tsx
+++ src/app/(route)/list/[id]/map/page.tsx
@@ -2,10 +2,13 @@ import { Suspense } from "react";
 import { DetailHeader } from "@/components/layout";
 import { PostDetailKakaoMap } from "./_components";
 
+const HEADER_TITLE = "분실/발견 위치";
+
 const page = () => {
   return (
     <section className="flex h-screen flex-col">
-      <DetailHeader title="분실/발견 위치" />
+      <DetailHeader title={HEADER_TITLE} />
+      <h1 className="sr-only">{HEADER_TITLE}</h1>
 
       <div className="min-h-0 flex-1">
         <Suspense fallback={null}>
