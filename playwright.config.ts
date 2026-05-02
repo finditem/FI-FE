eb9ff023927f9cf7c7f44efbaad096ba9a35dac1
wlrnjs <wlrnjs5824@gmail.com>
2026-05-02 18:00:24 +0900

    chore(setnry): E2E 테스트 동작 시 Sentry가 실행되지 않도록 구성

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,5 +57,8 @@ export default defineConfig({
     command: process.env.CI ? "npm run start" : "npm run dev",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
+    env: {
+      NEXT_PUBLIC_IS_E2E: "true",
+    },
   },
 });
