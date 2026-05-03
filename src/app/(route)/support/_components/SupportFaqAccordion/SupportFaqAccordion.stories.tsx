7573951b0e715953bf7508302780e1edfd81a675
jun <znznun@gmail.com>
2026-05-04 00:34:29 +0900

    test(support): SupportFaqAccordion 테스트, 스토리북 추가

---
+++ src/app/(route)/support/_components/SupportFaqAccordion/SupportFaqAccordion.stories.tsx
@@ -0,0 +1,63 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import SupportFaqAccordion from "./SupportFaqAccordion";
+
+const meta: Meta<typeof SupportFaqAccordion> = {
+  title: "페이지/자주 묻는 질문/SupportFaqAccordion",
+  component: SupportFaqAccordion,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        pathname: "/support",
+        query: { tab: "all" },
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div className="w-full max-w-[430px] bg-white">
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof SupportFaqAccordion>;
+
+export const Default: Story = {};
+
+export const AccountTab: Story = {
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: "/support",
+        query: { tab: "account" },
+      },
+    },
+  },
+};
+
+export const UsageTab: Story = {
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: "/support",
+        query: { tab: "usage" },
+      },
+    },
+  },
+};
+
+export const EtcTab: Story = {
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: "/support",
+        query: { tab: "etc" },
+      },
+    },
+  },
+};
