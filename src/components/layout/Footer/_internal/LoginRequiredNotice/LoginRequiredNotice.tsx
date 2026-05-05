22573d3fe0c5c50d573806e1f57587e5fb119b98
jun <znznun@gmail.com>
2026-05-05 17:16:00 +0900

    fix(footer): 코멘트 반영

--- src/components/layout/Footer/_internal/LoginRequiredNotice/LoginRequiredNotice.tsx
+++ src/components/layout/Footer/_internal/LoginRequiredNotice/LoginRequiredNotice.tsx
@@ -39,7 +39,7 @@ const LoginRequiredNotice = () => {
                   로그인 후 이용하실 수 있어요
                 </p>
                 <div
-                  className="pointer-events-none absolute left-1/2 top-full z-[1] h-[6px] w-[12px] -translate-x-1/2 -translate-y-px bg-fill-brand-subtle-default_2 [clip-path:polygon(50%_100%,0_0,100%_0)]"
+                  className="pointer-events-none absolute left-1/2 top-full h-[6px] w-[12px] -translate-x-1/2 -translate-y-px bg-fill-brand-subtle-default_2 [clip-path:polygon(50%_100%,0_0,100%_0)]"
                   aria-hidden
                 />
               </div>
