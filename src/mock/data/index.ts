8b000fd76f4e1a8c98aae4d656f8624f2ef00e4e
jun <znznun@gmail.com>
2026-05-05 16:24:40 +0900

    test(support-inquiryWrite-e2e): 공지사항, 1:1 문의 작성 e2e테스트 작성

--- src/mock/data/index.ts
+++ src/mock/data/index.ts
@@ -4,4 +4,5 @@ export * from "./posts.data";
 export * from "./userProfile.data";
 export * from "./admin.data";
 export * from "./comment.data";
+export * from "./inquiry.data";
 export * from "./notice.data";
