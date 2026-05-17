490e29de50af8fa2a8d629794aab0dabf3010b8a
jun <znznun@gmail.com>
2026-05-17 23:44:41 +0900

    docs(TSDoc): 누락 TSDoc 작성

--- src/constants/RELEASE_HOSTNAME.ts
+++ src/constants/RELEASE_HOSTNAME.ts
@@ -1,1 +1,8 @@
+/**
+ * release 환경의 `window.location.hostname`과 비교할 때 쓰는 호스트명 상수입니다.
+ *
+ * @author hyungjun
+ */
+
+/** SSE·채팅 소켓 등에서 로컬과 유사하게 same-origin 프록시를 쓰거나, 개발용 토큰 전달 분기 등에 `window.location.hostname`과 직접 비교합니다. */
 export const RELEASE_HOSTNAME = "finditem-release.vercel.app";
