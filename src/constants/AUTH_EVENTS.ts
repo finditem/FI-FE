b3ebd8e3634a9419986dd52cd04c1871b4f39e46
jun <znznun@gmail.com>
2026-05-18 01:54:26 +0900

    docs(TSDoc): TSDoc 설명 줄바꿈

--- src/constants/AUTH_EVENTS.ts
+++ src/constants/AUTH_EVENTS.ts
@@ -4,5 +4,7 @@
  * @author hyungjun
  */
 
-/** 이메일·카카오 로그인 등에서 `new CustomEvent(AUTH_LOGIN_SUCCESS_EVENT)`로 발생시키고, SSE 등 후속 연결 로직에서 `addEventListener`로 구독합니다. */
+/** 이메일·카카오 로그인 등에서 `new CustomEvent(AUTH_LOGIN_SUCCESS_EVENT)`로 발생시키고,
+ * SSE 등 후속 연결 로직에서 `addEventListener`로 구독합니다.
+ */
 export const AUTH_LOGIN_SUCCESS_EVENT = "authLoginSuccess";
