c822d447f8418a4ade715f5da4e7d91d810ce1ed
suhyeon0111 <soo4833@naver.com>
2026-05-26 15:01:28 +0900

    refactor(callback): pathname 인코딩 주소로 개선

--- src/app/authBootStrap.tsx
+++ src/app/authBootStrap.tsx
@@ -46,7 +46,9 @@ export default function AuthBootstrap() {
       onError: (error) => {
         logout();
         if (error.code === "AUTH401-INVALID_REFRESH" && isProtectPath) {
-          router.replace(`/login?reason=session-expired&callbackUrl=${pathname}`);
+          router.replace(
+            "/login?reason=session-expired&callbackUrl=" + encodeURIComponent(pathname)
+          );
         }
       },
       onSettled: () => {
